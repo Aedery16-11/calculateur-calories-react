38d257c5e3aef765283642e58eac5f8f8a606d6a
Aedery16-11 <aedery.16.11@gmail.com>
2026-01-27 20:57:09 +0100

    création de la classe CalorieEntry et début component TotalCalory

---
+++ web/src/components/TotalCalory.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { CaloryEntry } from '../models/CalorieEntry'
+
+const entries: CaloryEntry[] = [
+    {
+        label: "Pomme", category: "repas", qtyCalory: 50
+    },
+    {
+        label: "Pomme", category: "repas", qtyCalory: 50
+    }
+]
+const TotalCalory = () => {
+  return (
+    <div>TotalCalory</div>
+  )
+}
+
+export default TotalCalory
---
+++ web/src/models/CalorieEntry.tsx
@@ -0,0 +1,8 @@
+export type CaloryEntry = {
+    label: string,
+    qtyCalory: number
+    category: "sport" | "repas" //c'est soit l'un soit l'autre
+
+}
+
+//l'export est important pour pouvoir y accéder partout
