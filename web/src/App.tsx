c607d913e33cb83bbebb656ead1eea8352fbd239
Aedery16-11 <aedery.16.11@gmail.com>
2026-01-28 20:32:14 +0100

    ajout du composant total calory

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,11 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import TotalCalory from './components/TotalCalory'
 import './App.css'
 
 function App() {
-  
+  return <TotalCalory/>
 }
 
 export default App
--- web/src/components/TotalCalory.tsx
+++ web/src/components/TotalCalory.tsx
@@ -1,17 +1,33 @@
 import React from 'react'
-import { CaloryEntry } from '../models/CalorieEntry'
+import type { CaloryEntry } from '../models/CalorieEntry'
 
 const entries: CaloryEntry[] = [
-    {
-        label: "Pomme", category: "repas", qtyCalory: 50
-    },
-    {
-        label: "Pomme", category: "repas", qtyCalory: 50
-    }
+    { label: "Pomme", category: "repas", qtyCalory: 50 },
+    { label: "Jogging", category: "sport", qtyCalory: -200 }
 ]
+
 const TotalCalory = () => {
   return (
-    <div>TotalCalory</div>
+    <div className='overflow-x-auto rounded-box border border-base-content/5 bg-base-100'>
+        <table className="table">
+            <thead>
+                <tr>
+                    <th>Label</th>
+                    <th>Categorie</th>
+                    <th>Calories</th>
+                </tr>
+            </thead>
+            <tbody>
+                {entries.map((entry, index) => (
+                    <tr key={index}>
+                        <td>{entry.label}</td>
+                        <td>{entry.category}</td>
+                        <td>{entry.qtyCalory}</td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    </div>
   )
 }
 
