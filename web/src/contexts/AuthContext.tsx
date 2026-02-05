a8af278ec3ad1d3e4c3f8eced5f150661beede22
Aedery16-11 <aedery.16.11@gmail.com>
2026-02-05 12:26:01 +0100

    ajout de la persistence du token dans la fonction login et changement de son instanciation

--- web/src/contexts/AuthContext.tsx
+++ web/src/contexts/AuthContext.tsx
@@ -10,37 +10,44 @@ export type AuthContextType = {
 
 export const AuthContext = createContext<AuthContextType>({
   login: () => false,
-  logout: () => {},
+  logout: () => { },
 });
 
 export const AuthProvider = ({ children }: PropsWithChildren) => {
-
-  const [token, setToken] = useState<string>();
-
-  
+  const [token, setToken] = useState<string | undefined>(localStorage.getItem("token") || undefined);
+  //le token est soit de type string parce qu'il peut provenir du localstorage soit undefined si on a pas de token (le setItem est dans AuthContext fonction login)
   const login = async (email: string, password: string) => {
-   const response = await fetch('http://localhost:3000/auth/login', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ email, password }),
-    })
-    const json = await response.json()
-    setToken(json.token)
-  }
+    try {
+      const response = await fetch('http://localhost:3000/auth/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email, password }),
+      });
 
+      if (!response.ok) {
+        throw new Error("Erreur de connexion");
+      }
+
+      const json = await response.json();
+
+      localStorage.setItem("token", json.token);
+      setToken(json.token);
+
+    } catch (error) {
+      console.error(error);
+      alert("Erreur d'identifiants");
+    }
+  };
+
+  const logout = () => {
+    localStorage.removeItem("token");
+    setToken(undefined);
+  };
 
   return (
-    <AuthContext.Provider
-      value={{
-        token,
-        logout: () => {
-          setToken(undefined);
-        },
-        login,
-      }}
-    >
+    <AuthContext.Provider value={{ token, login, logout }}>
       {children}
     </AuthContext.Provider>
   );
